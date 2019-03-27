487901e8c770e1fe2c850de560fff6a684340e30
MoiOcanas <alejandro.ocanas@outlook.com>
2019-03-27 12:29:49 -0500

    Add changes on App.js to start to show posts

--- src/App.js
+++ src/App.js
@@ -2,24 +2,73 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Router
+import { Link } from 'react-router-dom';
+
+//Firebase
+import firebase from './Firebase';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.ref = firebase.firestore().collection('posts');
+    this.unsubcribe = null;
+
+    this.state = {
+      posts: []
+    }
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const posts = [];
+    querySnapshot.forEach(doc => {
+      const { title, description, author } = doc.data();
+      posts.push({
+        key: doc.id,
+        doc,
+        title,
+        description,
+        author,
+      });
+    });
+    this.setState({ posts });
+  }
+
+  componentDidMount() {
+    this.unsubcribe = this.ref.onSnapshot(this.onCollectionUpdate);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div class="container">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">
+              POST LIST
+            </h3>
+          </div>
+          <div class="panel-body">
+            <h4><Link to="/create">Add Post</Link></h4>
+            <table class="table table-stripe">
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Description</th>
+                  <th>Author</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.posts.map(post =>
+                  <tr>
+                    <td><Link to={`/show/${post.key}`}>{post.title}</Link></td>
+                    <td>{post.description}</td>
+                    <td>{post.author}</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
