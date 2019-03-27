71f1270cc42b384d3cf62d4ee1d8893e94d962a9
MoiOcanas <alejandro.ocanas@outlook.com>
2019-03-27 12:09:31 -0500

    Firebase configuration

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,1 +1,19 @@
 import * as firebase from 'firebase';
+import firestore from 'firebase/firestore';
+
+const settings = { timestampsInSnapshots: true};
+
+const config = {
+    apiKey: "AIzaSyD_3l_vHaaKrmXoho9-49WICfH-OoZ77po",
+    authDomain: "react-firebase-a93b8.firebaseapp.com",
+    databaseURL: "https://react-firebase-a93b8.firebaseio.com",
+    projectId: "react-firebase-a93b8",
+    storageBucket: "",
+    messagingSenderId: "535238636855"
+};
+
+firebase.initializeApp(config);
+
+firebase.firestore().settings(settings);
+
+export default firebase;
