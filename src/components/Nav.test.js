2f769833f9751bb994213b9b70cf2d928956d51e
MoiOcanas <alejandro.ocanas@outlook.com>
2019-03-28 00:43:41 -0500

    doint third unit test

--- src/components/Nav.test.js
+++ src/components/Nav.test.js
@@ -5,6 +5,13 @@ import { shallow } from 'enzyme';
 import Nav from './Nav';
 
 describe('Nav', () => {
+
+    it('should render correctly in "debug" mode', () => {
+        const component = shallow(<Nav debug />);
+      
+        expect(component).toMatchSnapshot();
+    })
+    
     it('it should have an li', () => {
         const wrapper = shallow(<Nav />)
         expect(wrapper.find('li')).toHaveLength(1);
