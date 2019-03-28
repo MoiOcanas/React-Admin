8d6ae186cc8beac4f7b44a40a78e239c8bdf3025
MoiOcanas <alejandro.ocanas@outlook.com>
2019-03-28 01:50:33 -0500

    Updates

---
+++ precache-manifest.1222fa8978ee705b61f157627fc30f02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e954c56d59c7d56b41c8",
+    "url": "/React-Admin/static/js/runtime~main.29d49ca7.js"
+  },
+  {
+    "revision": "c7c85b17497cd3d3aac3",
+    "url": "/React-Admin/static/js/main.4c7b1c69.chunk.js"
+  },
+  {
+    "revision": "0cf26acfc11d990a3c52",
+    "url": "/React-Admin/static/js/2.874abb61.chunk.js"
+  },
+  {
+    "revision": "c7c85b17497cd3d3aac3",
+    "url": "/React-Admin/static/css/main.26f22e1b.chunk.css"
+  },
+  {
+    "revision": "0cf26acfc11d990a3c52",
+    "url": "/React-Admin/static/css/2.0e59e0d2.chunk.css"
+  },
+  {
+    "revision": "0295fa8719fca5899f20b7a6c0b001a3",
+    "url": "/React-Admin/index.html"
+  }
+];
