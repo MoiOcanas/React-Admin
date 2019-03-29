00834cd65d225826f650f5f1473e330f55d3e50b
MoiOcanas <alejandro.ocanas@outlook.com>
2019-03-29 00:07:22 -0500

    adding process.env.public_url to Nav.js component

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ ReactDOM.render(
             <Route path={process.env.PUBLIC_URL +'/'} component={App} />
             <Route path={process.env.PUBLIC_URL +'/edit/:id'} component={Edit} />
             <Route path={process.env.PUBLIC_URL + '/create'} component={Create} />
-            <Route path={process.env.PUBLIC_URL+ '/show/:id'} component={Show} />
+            <Route path={process.env.PUBLIC_URL + '/show/:id'} component={Show} />
         </div>
     </Router>
     , document.getElementById('root'));
