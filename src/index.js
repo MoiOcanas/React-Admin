b5d115db4e12612237aaf642d9c8ed5fae7c3b9c
MoiOcanas <alejandro.ocanas@outlook.com>
2019-03-28 23:58:42 -0500

    first react-router test

--- src/index.js
+++ src/index.js
@@ -17,13 +17,13 @@ import Show from './components/Show';
 import Nav from './components/Nav';
 
 ReactDOM.render(
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
         <div>
             <Nav />
-            <Route path={process.env.PUBLIC_URL + '/'} component={App} />
-            <Route path={process.env.PUBLIC_URL + '/edit/:id'} component={Edit} />
+            <Route path={process.env.PUBLIC_URL +'/'} component={App} />
+            <Route path={process.env.PUBLIC_URL +'/edit/:id'} component={Edit} />
             <Route path={process.env.PUBLIC_URL + '/create'} component={Create} />
-            <Route path={process.env.PUBLIC_URL + '/show/:id'} component={Show} />
+            <Route path={process.env.PUBLIC_URL+ '/show/:id'} component={Show} />
         </div>
     </Router>
     , document.getElementById('root'));
